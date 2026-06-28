1ddf94cef429a0e81cc5fe55e7a06c537eb20c77
ncyu <cloud@ncyu.me>
2026-06-28 15:51:15 +0200

    Tighten GUI closed PnL time format
    Show closed-PnL UTC timestamps as month-day plus time, keep local display to time only, update cache-busting query strings, and extend the static GUI fill-history contract test.

--- program_code/exchange_connectors/bybit_connector/control_api_v1/app/static/common-formatters.js
+++ program_code/exchange_connectors/bybit_connector/control_api_v1/app/static/common-formatters.js
@@ -120,12 +120,11 @@ function ocFillDateTime(ts) {
   const d = ocDate(ts);
   if (!d) return '--';
   const p = n => String(n).padStart(2, '0');
-  const utcDate = d.getUTCFullYear() + '-' + p(d.getUTCMonth() + 1) + '-' + p(d.getUTCDate());
-  const localDate = d.getFullYear() + '-' + p(d.getMonth() + 1) + '-' + p(d.getDate());
+  const utcDay = p(d.getUTCMonth() + 1) + '-' + p(d.getUTCDate());
   let tz = '';
   try { const t = new Intl.DateTimeFormat(undefined, { timeZoneName: 'short' }).formatToParts(d).find(x => x.type === 'timeZoneName'); tz = t ? t.value : ''; } catch (_) {}
-  return utcDate + ' ' + p(d.getUTCHours()) + ':' + p(d.getUTCMinutes()) +
-    ' UTC (local: ' + localDate + ' ' + p(d.getHours()) + ':' + p(d.getMinutes()) +
+  return utcDay + ' ' + p(d.getUTCHours()) + ':' + p(d.getUTCMinutes()) +
+    ' UTC (local: ' + p(d.getHours()) + ':' + p(d.getMinutes()) +
     (tz ? ' ' + tz : '') + ')';
 }
 
